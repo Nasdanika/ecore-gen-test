c27c499a40c1209e37ec136ce57d2a70ea04cd2a
Pavel <Pavel.Vlasov@nasdanika.org>
2023-06-04 08:31:58 -0400

    EOperation and EParameter processor factory annotation tests

--- processors/src/main/java/org/nasdanika/html/ecore/gen/test/processors/AnimalProcessorsFactory.java
+++ processors/src/main/java/org/nasdanika/html/ecore/gen/test/processors/AnimalProcessorsFactory.java
@@ -10,7 +10,12 @@ import org.nasdanika.graph.processor.NodeProcessorConfig;
 import org.nasdanika.html.ecore.gen.processors.EAttributeNodeProcessor;
 import org.nasdanika.html.ecore.gen.processors.EClassNodeProcessor;
 import org.nasdanika.html.ecore.gen.processors.EClassifierNodeProcessorFactory;
+import org.nasdanika.html.ecore.gen.processors.EOperationNodeProcessor;
+import org.nasdanika.html.ecore.gen.processors.EOperationNodeProcessorFactory;
+import org.nasdanika.html.ecore.gen.processors.EParameterNodeProcessor;
+import org.nasdanika.html.ecore.gen.processors.EParameterNodeProcessorFactory;
 import org.nasdanika.html.ecore.gen.processors.EStructuralFeatureNodeProcessorFactory;
+import org.nasdanika.html.ecore.gen.test.TestPackage;
 import org.nasdanika.html.model.app.Action;
 import org.nasdanika.html.model.app.Label;
 import org.nasdanika.html.model.app.graph.Registry;
@@ -51,9 +56,10 @@ public class AnimalProcessorsFactory {
 	}
 	
 	@EStructuralFeatureNodeProcessorFactory(
-			name = "name",
+			featureID = TestPackage.ANIMAL__NAME,
 			label = "Имя",
-			description = "Имя животного, например 'Мурзик'"
+			description = "Имя животного, например 'Мурзик'",
+			documentation = "Test of attribute documentation"
 	)
 	public EAttributeNodeProcessor createAnimalNameProcessor(
 			NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config, 
@@ -73,6 +79,52 @@ public class AnimalProcessorsFactory {
 		};
 	}
 	
+	@EOperationNodeProcessorFactory(
+			operationID = TestPackage.ANIMAL___EATS__FOOD,
+			label = "Ест",
+			documentation = "Test of operation documentation"
+	)
+	public EOperationNodeProcessor createAnimalEatsProcessor(
+			NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config, 
+			java.util.function.Function<ProgressMonitor, Action> prototypeProvider,
+			BiConsumer<Label, ProgressMonitor> labelConfigurator,
+			ProgressMonitor progressMonitor) {		
+		return new EOperationNodeProcessor(config, context, prototypeProvider) {
+			
+			@Override
+			protected void configureLabel(EObject eObject, Label label, ProgressMonitor progressMonitor) {
+				super.configureLabel(eObject, label, progressMonitor);
+				if (labelConfigurator != null) {
+					labelConfigurator.accept(label, progressMonitor);
+				}
+			}
+			
+		};
+	}
 	
+	@EParameterNodeProcessorFactory(
+			operationID = TestPackage.ANIMAL___EATS__FOOD,
+			name = "food",
+			label = "пища",
+			description = "Food which animal eats",
+			documentation = "Test of parameter documentation"
+	)
+	public EParameterNodeProcessor createAnimalEatsFoodProcessor(
+			NodeProcessorConfig<Object, WidgetFactory, WidgetFactory, Registry<URI>> config, 
+			java.util.function.Function<ProgressMonitor, Action> prototypeProvider,
+			BiConsumer<Label, ProgressMonitor> labelConfigurator,
+			ProgressMonitor progressMonitor) {		
+		return new EParameterNodeProcessor(config, context, prototypeProvider) {
+			
+			@Override
+			protected void configureLabel(EObject eObject, Label label, ProgressMonitor progressMonitor) {
+				super.configureLabel(eObject, label, progressMonitor);
+				if (labelConfigurator != null) {
+					labelConfigurator.accept(label, progressMonitor);
+				}
+			}
+			
+		};
+	}
 
 }
